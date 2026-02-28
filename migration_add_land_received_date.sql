79b6d805e135cd0fc31987d385611d8aba65363e
Rajesh <rajeshsarkar81704@gmail.com>
2026-02-28 11:31:49 +0530

    removed-some-files

--- migration_add_land_received_date.sql
+++
@@ -1,5 +0,0 @@
--- Migration: Add LAND_RECEIVED_DATE column to TENDER_DATA_DTLS table
--- Run this if your database already exists
-
-ALTER TABLE tender."TENDER_DATA_DTLS" 
-ADD COLUMN IF NOT EXISTS "LAND_RECEIVED_DATE" varchar NULL;
